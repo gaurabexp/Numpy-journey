1c483d42b695e1206de9a53a2e2e55fc56391816
gaurabexp <gaurabexp@gmail.com>
2026-03-23 23:18:45 +0545

    Add 3D array examples and initialization of zero arrays

--- basics_02.py
+++ basics_02.py
@@ -33,8 +33,20 @@ a[:, 2] =[1,2]
 print(a)
 
 
+#3d example
+b=np.array([[[1,2],[3,4]],[[5,6],[7,8]]])
+print(b)
 
+#get specific element (works outside in)
+print(b[0,1,1])    # 0-> first index of the 3d array [[1,2],[3,4]] , 1--> second row , 1--> second element (i.e 4)
 
+print(b[0,1,:])         # [3,4]  print index 1 row from 1st array index
+print(b[0,:,0])         #[1,3]   Print intex 0 column from 1st array index
+
+#replace
+b[:,1,:] =[[9,9],[8,8]]  # this changes the value of both row index of 0 and 1 of arrays
+# b[:,1,:] =[[9,9,9],[8,8]]  #this doesnot work
+print(b)
 
 #get class type
 print(type(a))       #---><class 'numpy.ndarray'>
---
+++ basics_03.py
@@ -0,0 +1,6 @@
+# Initializing Different Types of Arrays
+import numpy as np
+
+#print all zeros
+a= np.zeros(5)
+print(a)
