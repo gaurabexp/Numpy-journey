a08cf215bac0ea68a520cd2704e72db3ac3bf2e7
gaurabexp <gaurabexp@gmail.com>
2026-03-24 16:25:11 +0545

    Fix formatting and add missing newlines in array initialization examples

--- basics_03.py
+++ basics_03.py
@@ -4,3 +4,15 @@ import numpy as np
 #print all zeros
 a= np.zeros(5)
 print(a)
+
+#print  2x 3 matrix
+b= np.zeros((2,3))
+print(b)
+
+#priint 3d matrix
+c= np.zeros((2,3,3))
+print(c)
+
+#all ones matrix
+d=np.ones((2,3,3))
+print(d)
