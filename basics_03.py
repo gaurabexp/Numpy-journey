d160a4786c4d7cafe41004f32c6b31fa680d2bd0
gaurabexp <gaurabexp@gmail.com>
2026-03-24 20:41:09 +0545

    Refactor array initialization examples and add random number generation sections

--- basics_03.py
+++ basics_03.py
@@ -13,6 +13,38 @@ print(b)
 c= np.zeros((2,3,3))
 print(c)
 
-#all ones matrix
+#all 1s matrix
 d=np.ones((2,3,3))
 print(d)
+
+
+#Any other number
+e= np.full((2,2),99, dtype='float32')
+print(e)               #---->[[99. 99.]
+                        #     [99. 99.]]
+print(e.dtype)
+
+
+#Any other number(full_like)
+f=np.full_like(a,4)
+print(f)            #--->[4. 4. 4. 4. 4.]
+
+
+
+#random decimal numbers
+g=np.random.rand(4,2)
+h=np.random.random_sample(a.shape)
+print(g)
+print(h)
+
+
+
+
+#random Integer values
+i=np.random.randint(7)
+j=np.random.randint(7, size=(3,3))
+print(i)    #this gives one value , so we need to give size to from a proper matrix
+print(j)
+
+
+
