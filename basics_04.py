b738c1b8e519ff975085ea23319ab38433003b5c
gaurabexp <gaurabexp@gmail.com>
2026-03-26 17:38:30 +0545

    Add linear algebra and statistics examples in basics_04.py

--- basics_04.py
+++ basics_04.py
@@ -16,3 +16,35 @@ print(a**2)
 #take the sin 
 si=np.sin (a)
 print(si)
+
+
+
+#linear Algebra
+aa= np.ones((2,3))
+print(aa)
+
+bb=np.full((3,2),2)
+print(bb)
+
+print(np.matmul(aa,bb))
+
+#find the determinant
+c=np.identity(3)
+print(np.linalg.det(c))
+
+
+
+### for more informations
+# https://numpy.org/doc/stable/reference/routines.math.html
+
+
+
+# Statistics
+stats=np.array([[1,2,3],[4,5,6]])
+print(stats)
+
+print(np.min(stats))
+print(np.min(stats, axis=1))
+print(np.max(stats))
+print(np.sum(stats))
+print(np.sum(stats, axis=0))
