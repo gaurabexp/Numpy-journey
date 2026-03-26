ac3d0e0db94132fa78e1b59e7d285a67465b7699
gaurabexp <gaurabexp@gmail.com>
2026-03-26 18:17:52 +0545

    Add examples for array reshaping and stacking in basics_05.py; create basics_06.py for miscellaneous functions

---
+++ basics_05.py
@@ -0,0 +1,23 @@
+import numpy as np
+#reorganizing Arrays
+before=np.array([[1,2,3,4],[5,6,7,8]])
+print(before)
+print(before.shape)
+
+
+after = before.reshape((8,1))
+print(after)
+
+
+# vertically stacking vectors
+v1= np.array([1,2,3,4])
+v2= np.array([5,6,7,8])
+
+print(np.vstack([v1,v2,v1,v2]))
+
+
+#Horizontal stacking
+h1= np.ones((2,4))
+h2= np.zeros((2,2))
+
+print(np.hstack((h1,h2)))
---
+++ basics_06.py
@@ -0,0 +1,5 @@
+import numpy as np
+
+#miscellaneous
+
+#load data from files
